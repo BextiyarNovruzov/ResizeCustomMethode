1b68cc820e8130213040eef913614d4724471fc6
BextiyarNovruzov <baxtiyarbn-bp215@code.edu.az>
2024-10-17 05:01:50 +0400

    Stack & Heap, Value & Reference types, Ref, Out--Task

---
+++ ResizeCustom.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3 }; 
+        InsertArray(ref numbers, 4, 5, 6);  
+   }
+
+    static void InsertArray(ref int[] array, params int[] values)
+    {
+        int originalLength = array.Length;  
+        int newLength = originalLength + values.Length;  
+        int[] newArray = new int[newLength];
+
+        for (int i = 0; i < originalLength; i++)
+        {
+            newArray[i] = array[i];
+        }
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            newArray[originalLength + i] = values[i];
+        }
+
+        array = newArray;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
